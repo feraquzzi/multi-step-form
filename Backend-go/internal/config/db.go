799f2610be4c11dbffb74ba588e53cf5d72306c5
feraquzzi <mosesobembe@gmail.com>
2026-04-19 13:07:13 +0100

    worked on the main.go

--- Backend-go/internal/config/db.go
+++ Backend-go/internal/config/db.go
@@ -9,23 +9,28 @@ import (
 
 var DB *sql.DB
 
-func ConnectDB() error{
+func ConnectDB() error {
 	var err error
 
 	dsn := "king/king123@localhost:1521/xepdb1"
 
 	DB, err = sql.Open("godror", dsn)
 	if err != nil {
-		log.Fatal("Failed to Connect to the database", err)
+		log.Println("Failed to connect to database:", err)
 		return err
-	}	
+	}
 
 	err = DB.Ping()
 	if err != nil {
-		log.Fatal("Failed to Ping to the database", err)
-	return err
+		log.Println("Failed to ping database:", err)
+		return err
 	}
 
+	log.Println("Database connected successfully")
 	return nil
-
 }
+
+// func ConnectDB() error {
+// 	log.Println("Skipping DB for test")
+// 	return nil
+// }
